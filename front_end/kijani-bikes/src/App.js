57bb812d4b31f91a3b16f56462b7b6d870dddd2c
apha-pesso <mutiuayo99@gmail.com>
2023-02-23 14:37:23 +0100

    Add about page

--- front_end/kijani-bikes/src/App.js
+++ front_end/kijani-bikes/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import About from "./pages/About";
 
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
