5a16ff14a6a97aa09caece002c633ad925f5f219
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-02-25 14:19:54 +0100

    Add dump with bike

--- back_end/dump.sql
+++ back_end/dump.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.41, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.41, for osx10.18 (x86_64)
 --
 -- Host: localhost    Database: kijani_test_db
 -- ------------------------------------------------------
@@ -44,6 +44,34 @@ LOCK TABLES `areas` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `bikes`
+--
+
+DROP TABLE IF EXISTS `bikes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bikes` (
+  `type` varchar(128) NOT NULL,
+  `status` varchar(128) NOT NULL,
+  `location` geometry DEFAULT NULL,
+  `id` varchar(60) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bikes`
+--
+
+LOCK TABLES `bikes` WRITE;
+/*!40000 ALTER TABLE `bikes` DISABLE KEYS */;
+INSERT INTO `bikes` VALUES ('Mountain Bike','Available',_binary '\0\0\0\0\0\0\0\0\0\0\0\0\0$@\0\0\0\0\0\04@','47a44b11-3bf9-4a73-b467-2516ac48f2b5','2023-02-25 12:54:50','2023-02-25 12:54:50');
+/*!40000 ALTER TABLE `bikes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `cities`
 --
 
@@ -67,6 +95,90 @@ LOCK TABLES `cities` WRITE;
 /*!40000 ALTER TABLE `cities` DISABLE KEYS */;
 INSERT INTO `cities` VALUES ('London','1d4fcfe4-3dd2-4649-a7ec-3c952f3077a7','2023-02-24 12:23:38','2023-02-24 12:23:38'),('Kuala','43d34020-24c4-4f6d-aec4-096977f5f1b9','2023-02-24 12:54:01','2023-02-24 13:40:00'),('Lagos','5d9f7611-5f04-44d0-830b-1797f02b6210','2023-02-24 10:53:09','2023-02-24 10:53:09');
 /*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `racks`
+--
+
+DROP TABLE IF EXISTS `racks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `racks` (
+  `station_id` varchar(128) NOT NULL,
+  `id` varchar(60) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `racks`
+--
+
+LOCK TABLES `racks` WRITE;
+/*!40000 ALTER TABLE `racks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `racks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stations`
+--
+
+DROP TABLE IF EXISTS `stations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stations` (
+  `name` varchar(128) NOT NULL,
+  `area_id` varchar(60) NOT NULL,
+  `description` varchar(128) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `rack_id` varchar(60) NOT NULL,
+  `location` geometry NOT NULL,
+  `id` varchar(60) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stations`
+--
+
+LOCK TABLES `stations` WRITE;
+/*!40000 ALTER TABLE `stations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trips`
+--
+
+DROP TABLE IF EXISTS `trips`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trips` (
+  `duration` varchar(128) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `start_location` geometry NOT NULL,
+  `destination` geometry NOT NULL,
+  `id` varchar(60) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trips`
+--
+
+LOCK TABLES `trips` WRITE;
+/*!40000 ALTER TABLE `trips` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trips` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -111,4 +223,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-24 16:54:06
+-- Dump completed on 2023-02-25 14:18:49
