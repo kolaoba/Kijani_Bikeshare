323b96754f1ef4fa6b91e33d841361934d92d5eb
MissTipo <dorine.a.tipo@gmail.com>
2023-02-28 07:46:53 +0300

    Dashboard with dropdown menu update

---
+++ front_end/kijani-bikes/src/components/SidebarData.js
@@ -0,0 +1,92 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
+
+export const SidebarData = [
+{
+	title: "Profile",
+	path: "/profile",
+	icon: <AiIcons.AiFillHome />,
+	iconClosed: <RiIcons.RiArrowDownSFill />,
+	iconOpened: <RiIcons.RiArrowUpSFill />,
+
+	subNav: [
+	{
+		title: "Account Info",
+		path: "/profile/info",
+		icon: <IoIcons.IoIosPaper />,
+	},
+	{
+		title: "Transaction",
+		path: "/profile/transaction",
+		icon: <IoIcons.IoIosPaper />,
+	},
+	],
+},
+{
+	title: "Rides",
+	path: "/rides",
+	icon: <IoIcons.IoIosPaper />,
+	iconClosed: <RiIcons.RiArrowDownSFill />,
+	iconOpened: <RiIcons.RiArrowUpSFill />,
+
+	subNav: [
+	{
+		title: "Enter Current Location",
+		path: "/rides/location",
+		icon: <IoIcons.IoIosPaper />,
+		cName: "sub-nav",
+	},
+	{
+		title: " UnlockBike",
+		path: "/rides/unlock",
+		icon: <IoIcons.IoIosPaper />,
+		cName: "sub-nav",
+	},
+	{
+		title: "Enter Destination",
+		path: "/rides/destination",
+		icon: <IoIcons.IoIosPaper />,
+	},
+	{
+        	title: "Dock",
+        	path: "/rides/dock",
+        	icon: <IoIcons.IoIosPaper />,
+	},
+	],
+},
+{
+	title: "Contact",
+	path: "/contact",
+	icon: <FaIcons.FaPhone />,
+},
+{
+	title: "Trips",
+	path: "/trips",
+	icon: <FaIcons.FaEnvelopeOpenText />,
+
+	iconClosed: <RiIcons.RiArrowDownSFill />,
+	iconOpened: <RiIcons.RiArrowUpSFill />,
+
+	subNav: [
+	{
+		title: "Recent Trips",
+		path: "/trips/recent",
+		icon: <IoIcons.IoIosPaper />,
+	},
+	{
+		title: "Trip Rewards",
+		path: "/trips/rewards",
+		icon: <IoIcons.IoIosPaper />,
+	},
+	],
+},
+{
+	title: "Settings",
+	path: "/settings",
+	icon: <IoIcons.IoMdHelpCircle />,
+},
+];
+
---
+++ front_end/kijani-bikes/src/pages/Profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Profile = () => {
+return (
+	<div className="home">
+	<h1>Kijani Bikes User Profile</h1>
+	</div>
+);
+};
+
+export const AccountInfo = () => {
+return (
+	<div className="home">
+	<h1>Kijani basic user info</h1>
+	</div>
+);
+};
+
+export const Transactions = () => {
+return (
+	<div className="home">
+	<h1>Transaction and payment history</h1>
+	</div>
+);
+};
+
