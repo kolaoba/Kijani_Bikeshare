7592529f3c733ca46d2ff15ec011f9f3539311aa
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-03-07 12:37:18 +0100

    modify trips table add calculated nullable duration add nullable end_time

--- back_end/models/trip.py
+++ back_end/models/trip.py
@@ -3,18 +3,35 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, DateTime, Integer
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class Trip(BaseModel, Base):
     """Represents Trip class"""
     __tablename__ = 'trips'
-    duration = Column(Integer, nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     bike_id = Column(String(60), ForeignKey('bikes.id'), nullable=False)
     start_time = Column(DateTime, nullable=False)
-    end_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=True)
     start_docking_station = Column(String(60), ForeignKey('stations.id'), nullable=False)
     destination_docking_station = Column(String(60), ForeignKey('stations.id'), nullable=False)
+
+    duration = Column(Integer, nullable=True)
     
     def __init__(self, *args, **kwargs):
         """initializes Trip"""
         super().__init__(*args, **kwargs)
+        
+    @hybrid_property
+    def duration_seconds(self):
+        diff = (self.end_time - self.start_time).total_seconds()
+        return diff
+
+    @validates('end_time', 'start_time')
+    def validate_end_time(self, key, end_time):
+        if key == 'end_time':
+            assert end_time >= self.start_time, 'End time must be after start time'
+            self.duration = self.duration_seconds
+        elif key == 'start_time':
+            assert end_time <= self.end_time, 'Start time must be before end time'
+        return end_time    
